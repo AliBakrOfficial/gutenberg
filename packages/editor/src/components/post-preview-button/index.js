2ff4b46f7370deb6824918e11297467954285588
Riad Benguella <benguella@gmail.com>
2018-08-15 13:54:34 +0100

    Fix simultaneous click on the preview button

--- packages/editor/src/components/post-preview-button/index.js
+++ packages/editor/src/components/post-preview-button/index.js
@@ -64,9 +64,10 @@ export class PostPreviewButton extends Component {
 		// Open a popup, BUT: Set it to a blank page until save completes. This
 		// is necessary because popups can only be opened in response to user
 		// interaction (click), but we must still wait for the post to save.
-		if ( ! this.previewWindow ) {
+		if ( ! this.previewWindow || this.previewWindow.closed ) {
 			this.previewWindow = window.open( '', this.getWindowTarget() );
 		}
+		this.previewWindow.focus();
 
 		// If there are no changes to autosave, we cannot perform the save, but
 		// if there is an existing preview link (e.g. previous published post
